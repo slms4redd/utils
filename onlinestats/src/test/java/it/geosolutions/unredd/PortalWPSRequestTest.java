8eee5a7d600fa7caa91356fb7bbc39be61f84a73
Oscar Fonts <oscar.fonts@gmail.com>
2012-12-20 18:51:42 +0100

    Updated portal WPS request test & sample data; now it works

--- onlinestats/src/test/java/it/geosolutions/unredd/PortalWPSRequestTest.java
+++ onlinestats/src/test/java/it/geosolutions/unredd/PortalWPSRequestTest.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.geoserver.wps.WPSTestSupport;
+import org.geotools.TestData;
 import org.junit.Test;
 import com.mockrunner.mock.web.MockHttpServletResponse;
 
@@ -13,6 +14,10 @@ public class PortalWPSRequestTest extends WPSTestSupport {
 	public void test() throws Exception {
 	      InputStream is = this.getClass().getResourceAsStream("portal_execute.xml");
 	      String xml = IOUtils.toString(is, "UTF-8");
+	      xml = xml.replace("{AREA}", TestData.file(this, "area.tif").getAbsolutePath());
+	      xml = xml.replace("{PROVINCES}", TestData.file(this, "provinces.tif").getAbsolutePath());
+	      xml = xml.replace("{FOREST_MASK}", TestData.file(this, "forest_mask.tif").getAbsolutePath());
+	      System.out.println(xml);
 	      MockHttpServletResponse response = postAsServletResponse("wps?", xml);
 	      System.out.println(response.getOutputStreamContent());
 	}
